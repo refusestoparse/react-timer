1fb693ce713699d9ff0538c539f8eecb175757e3
refusestoparse <charlie.akin2016@gmail.com>
2024-08-18 14:14:26 -0700

    created timer functionality and it updates. Times do not read as correct yet.

--- react-timer/src/App.tsx
+++ react-timer/src/App.tsx
@@ -1,33 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [seconds, setSeconds] = useState(0)
+  const [minutes, setMinutes] = useState(0)
+  const [hours, setHours] = useState(0)
+  const start = new Date("2024-08-18").getTime()
+  const [time, setTime] = useState(start)
+  let run=false
+  useEffect(() => {
+   const interval = setInterval(() => {
+      startTimer()
+  
+   }, 1000);
 
+   return () => clearInterval(interval);
+ }, []);
+  
+  
+ function startTimer(){
+   setTime(Date.now);
+   console.log("handling state...")
+      let now =Date.now()
+     let  second = now-start
+  
+     setSeconds(second/1000)
+     // setMinutes(minute)
+     // setHours(hour)
+  
+  
+   console.log("State handled")
+   
+  }
+
+
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>{hours} : {minutes} : {seconds}</h1>
+      <button onClick={startTimer}>Start</button>
+      <button>Stop</button>
     </>
   )
 }
